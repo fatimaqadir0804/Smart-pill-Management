c3b54f1205f5a89c543c82f3a57115880353e275
fatimaqadir0804 <fatimaqadirtts@gmail.com>
2026-06-03 15:20:50 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/app.js',
+  '/style.css'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
